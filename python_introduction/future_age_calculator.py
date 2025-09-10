6e915919f876f36107556c8cef6be89e79cd71a2
quol <kuolamol77@gmail.com>
2025-09-10 21:25:13 +0300

    completed the tasks

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,10 @@
+# Practice receiving user input in Python and perform a simple arithmetic operation to calculate the user’s age in a future year.
+
+current_age = int(input("How old are you? : "))
+
+future_year = 2050
+
+age_difference = future_year - 2023
+age = current_age + age_difference
+
+print(f"In {future_year}, you will be {age} years old.")
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,7 @@
+# Demonstrate understanding of variable assignments and arithmetic operations by converting a given number of hours into seconds.
+
+hours = 2
+seconds_in_an_hour = 3600
+
+seconds = hours * seconds_in_an_hour
+print(f"{hours} hours is {seconds} seconds.")
