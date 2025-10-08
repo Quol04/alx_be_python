1eaaf5af5a915ca672a60d01e9a598d5ed3d4436
quol <kuolamol77@gmail.com>
2025-10-08 17:13:10 +0300

    modified the library_system files

---
+++ oop/library_system.py
@@ -0,0 +1,40 @@
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        return self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            print(book)
+
+
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    # Book: Pride and Prejudice by Jane Austen
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"  
+
+   
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size  # in KB
+
+    #  Snow Crash by Neal Stephenson, File Size: 500KB
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+class PrintBook(Book):
+    def __init__(self, title, author, num_pages):
+        super().__init__(title, author)
+        self.num_pages = num_pages
+     
+     #PrintBook: The Catcher in the Rye by J.D. Salinger, Page Count: 234
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.num_pages}"
